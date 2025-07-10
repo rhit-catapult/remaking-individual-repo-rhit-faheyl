580ec204250481e25cea9d39e6092fe578f34f49
rhit-harmgc <harmgc@rose-hulman.edu>
2025-07-10 10:51:26 -0400

    Evil dog bark

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -6,6 +6,7 @@ def main():
     # pre-define RGB colors for Pygame
     BLACK = pygame.Color("Black")
     WHITE = pygame.Color("White")
+    RED = pygame.Color("Red")
     IMAGE_SIZE = 470
     TEXT_HEIGHT = 30
 
@@ -19,28 +20,35 @@ def main():
     # Prepare the image
     # TODO 1: Create an image with the 2dogs.JPG image
     # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
-
+    image1 = pygame.image.load("2dogs.JPG")
+    image1 = pygame.transform.scale(image1, (470, 470))
     # Prepare the text caption(s)
     # TODO 4: Create a font object with a size 28 font.
+    font1 = pygame.font.SysFont("Roboto", 28)
+    font2 = pygame.font.SysFont("comicsansms", 40)
     # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
-
+    caption1 = font1.render("Two Dawgs", True, BLACK)
+    caption2 = font2.render("im evil dog >:)", True, RED)
     # Prepare the music
     # TODO 8: Create a Sound object from the "bark.wav" file.
-
+    bark_sound = pygame.mixer.Sound("bark.wav")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             # TODO 9: Play the music (bark) if there's a mouse click.
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bark_sound.play()
         # Clear the screen and set the screen background
         screen.fill(WHITE)
 
         # Draw the image onto the screen
         # TODO 2: Draw (blit) the image onto the screen at position (0, 0)
-
+        screen.blit(image1, (0, 0))
         # Draw the text onto the screen
         # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
+        screen.blit(caption1, (170, 475))
+        screen.blit(caption2, (220, 10))
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
 
